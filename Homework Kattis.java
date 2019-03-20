5d4fd6af23c986ff675f834bcc05eb47bb763e8e
Leoalmang <42684126+Leoalmang@users.noreply.github.com>
2019-03-20 22:37:23 +0100

    Update and rename Homework Kattis to Homework Kattis.java

--- Homework Kattis.java
+++ Homework Kattis.java
@@ -7,43 +7,36 @@ public class SecureDoors {
     {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
-
         ArrayList<String> list = new ArrayList<>();
-
         while (n-- > 0)
         {
             String[] line = scanner.nextLine().split(" ");
-
             String command = line[0];
             String name = line[1];
-
             if(command.equals("entry"))
             {
-                boolean found = false;
-                for(int i = 0; i < list.size(); i++) {
-                    if(list.get(i).equals(name)) {
-                        found = true;
-                        System.out.printf("%s entered (ANOMALY)\n", name);
+             boolean found = false;
+             for(int i = 0; i < list.size(); i++) {
+             if(list.get(i).equals(name)) {
+             found = true;
+             System.out.printf("%s entered (ANOMALY)\n", name);
 
-                        break;
-                    }
-                }
-
+                        break;}
+    }
                 if(!found) {
                     list.add(name);
                     System.out.printf("%s entered\n", name);
-                }
-            } else if(command.equals("exit")) {
+                             }
+                
+                } else if(command.equals("exit")) {
                 boolean found = false;
                 for(int i = 0; i < list.size(); i++) {
-                    if(list.get(i).equals(name)) {
-                        found = true;
-                        list.remove(i);
-                        System.out.printf("%s exited\n", name);
-                        break;
-                    }
+                if(list.get(i).equals(name)) {
+                found = true;
+                list.remove(i);
+                System.out.printf("%s exited\n", name);
+                break; }
                 }
-
                 if(!found) {
                     System.out.printf("%s exited (ANOMALY)\n", name);
                 }
